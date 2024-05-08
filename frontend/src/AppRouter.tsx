d36a08075991db5dcbaf96d87c107765e514bc32
Etienne Rousseau <etar2004@gmail.com>
2024-05-07 21:11:50 -0700

    Added App Router

---
+++ frontend/src/AppRouter.tsx
@@ -0,0 +1,21 @@
+// import React from 'react';
+// import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+// import MarketPlace from './pages/MarketPlace';  // Adjust the path as needed
+//      // Adjust the path as needed
+
+// const AppRouter: React.FC = () => {
+//     return (
+//         <Router>
+//             <Switch>
+//                 <Route exact path="/" render={() => <Redirect to="/market" />} />
+//                 <Route path="/market" component={MarketPlace} />
+//                 <Route path="/form" component={FormPage} />
+//                 <Route path="/login" component={LoginPage} />
+//                 {/* Optionally add a route for handling 404 Not Found */}
+//                 <Route path="*" render={() => <div>404 Not Found</div>} />
+//             </Switch>
+//         </Router>
+//     );
+// };
+
+// export default AppRouter;
