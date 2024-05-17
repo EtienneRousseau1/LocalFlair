6f6f582af64d082f3d3572fe93db94ea590c7e56
Etienne Rousseau <etar2004@gmail.com>
2024-05-17 10:50:08 -0700

    MOre Testing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,7 +6,7 @@ import Marketplace from './pages/MarketPlace';
 import AppRouter from './AppRouter';  
 
 const App: React.FC = () => {
-  // Testing out Commits
+  // Testing out Commits Continued
     return (
         <div>  
 
